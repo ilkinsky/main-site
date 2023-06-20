0261c8c1c5204200a0d379ce7e9c2d34ad50a2fb
Frozexxn <106523171+ilkinsky@users.noreply.github.com>
2023-06-20 22:02:48 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Portfolio made by [Aronshire](https://github.com/Aronshire)
+
+## 📝 Credits
+
+ - [Next.js](https://nextjs.org/)
+ - [Tailwind CSS](https://tailwindcss.com/)
+
+ Description: This is my portfolio website. I made it with Next.js and Tailwind CSS. I used Next.js for the server side rendering and Tailwind CSS for the styling. I used the Next.js with Tailwind CSS
+
+## 📦 Installation
+
+```bash 
+git clone https://github.com/Aronshire/portfolio.git
+```
+
+## 🛠 Setup
+
+```bash
+cd portfolio
+npm install --s --f && npm run dev
+```
+
+## 📄 License
+
+MIT © [Aronshire](./LICENSE)
---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    discord: {
+        id: "994209718585266299",
+    },
+    github: {
+        token1: "ghp_glp5HVMRtuOmz6b",
+        token2: "sQrU6hpJNdqo10J2IkmuF"
+    },
+    kaily: "05/05/2024",
+    about: {
+        description: "Hello, I'm Ilkin, 13 year old developer living in Azerbaijan and im learning JavaFX.",
+    },
+    social: {
+        github: "https://github.com/ilkinsky",
+        instagram: "https://www.instagram.com/akhmadzzada/",
+        discord: "https://discord.com/users/994209718585266299",
+        spotify: "https://open.spotify.com/user/31tmlrhwk27bggahkkiegoid3g5q",
+    },
+    meta: {
+        title: 'My Personal Website',
+        description: 'My Personal Website',
+        theme: '#02060f',
+        lang: 'en',
+        url: 'https://aronshire.com.tr',
+        image: '',
+    },
+    title: 'Frozexxn',
+    titles: [
+        { title: 'Home', url: '/', icon: 'fa-house', invisible: false },
+        { title: 'Projects', url: '/projects', icon: 'fa-blog', invisible: false },
+    ]
+}
+
